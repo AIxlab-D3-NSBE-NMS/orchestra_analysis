72938dd214f3ae9be24f851d091ece48b6245394
diogoflduarte <diogo.fl.duarte@gmail.com>
2026-03-06 16:00:25 +0000

    added two features but vlc opens fullscreen

--- prr/annotate_videos.py
+++ prr/annotate_videos.py
@@ -1,14 +1,20 @@
 import pandas as pd
 from vlc import Instance
+from pathlib import Path
+import pdb
 
 # Initialize VLC instance
 vlc_instance = Instance()
 
-def open_and_input(row):
-    video_path = row['filepath']
-    status_website = row['status_website']
-    submitted = row['submitted']
-    tentativas = row['tentativas']
+def open_and_input(row, basepath):
+    video_path      = basepath / Path(row['filepath'])
+    breakpoint()
+    status_website  = row['status_website']
+    login           = row['login']
+    submitted       = row['submitted']
+    tentativas      = row['tentativas']
+    qualtrics       = row['qualtrics']
+
 
     # Open the video file with VLC
     player = vlc_instance.media_player_new()
@@ -17,12 +23,18 @@ def open_and_input(row):
     player.play()
 
     try:
-        login = input(f"Login for {video_path}: ")
-        qualtrics = input(f"Qualtrics for {video_path}: ")
+        status_website      = input(f"Status do website: ")
+        login               = input(f"Login : ")
+        submitted           = input(f"Conseguiu submeter: ")
+        tentativas          = input(f"Tentativas: ")
+        qualtrics           = input(f"Qualtrics: ")
 
         # Update the row with new inputs
-        row['login'] = login
-        row['qualtrics'] = qualtrics
+        row['status_website']   = status_website
+        row['login']            = login
+        row['submitted']        = submitted
+        row['tentativas']       = tentativas
+        row['qualtrics']        = qualtrics
 
     finally:
         # Stop and release VLC resources
@@ -31,15 +43,15 @@ def open_and_input(row):
 
 def main():
     file_path = '/home/labadmin/aixlab/code/orchestra_analysis/prr/cyclesix_week1_updated.csv'
-
+    basepath = Path('/media/labadmin/Windows/Users/diogo/aixlab/data/raw/')
     # Load the CSV file into a DataFrame
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path, sep='\t')
 
     for idx, row in df.iterrows():
-        if pd.isna(row['submitted']):
+        if pd.isna(row['status_website']):
             print(f"Processing video {idx} of {len(df) - 1}")
 
-            open_and_input(row)
+            open_and_input(row, basepath)
 
             # Save the updated DataFrame after each input
             df.to_csv(file_path, index=False)
