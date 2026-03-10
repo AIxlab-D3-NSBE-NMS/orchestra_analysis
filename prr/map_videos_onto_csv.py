b0530e2f67c139334a8c666386f7fed82f1157d5
diogoflduarte <diogo.fl.duarte@gmail.com>
2026-03-10 16:37:51 +0000

    script to make empty csv file from videos to be annotated

---
+++ prr/map_videos_onto_csv.py
@@ -0,0 +1,61 @@
+import os
+import pandas as pd
+from pathlib import Path
+from tqdm import tqdm
+from datetime import datetime
+import pdb
+def get_video_paths(input_folder, match_string=None):
+    video_paths = []
+
+    for root, _, files in os.walk(input_folder):
+        for file in files:
+            if match_string is None or match_string.lower() in file.lower():
+                full_path = os.path.join(root, file)
+                video_paths.append(full_path)
+
+    return video_paths
+
+def main(input_folder, output_csv, match_string=None):
+    video_paths = get_video_paths(input_folder, match_string)
+
+    df = pd.DataFrame(video_paths, columns=['filepath'])
+
+    # Extract date and time from the filename
+    readable_dates = []
+    readable_times = []
+
+    for path in tqdm(df['filepath']):
+
+        base_name = Path(path).stem # only filename and without extension
+
+        # assuming appended date and time in the format: '20261231_181735'
+        date_string = base_name[-15:-7]
+        time_string = base_name[-6:]
+
+
+        date_obj = datetime.strptime(date_string, "%Y%m%d")
+        readable_date = date_obj.strftime("%d.%m.%Y")
+
+        time_obj = datetime.strptime(time_string, "%H%M%S")
+        readable_time = time_obj.strftime("%Hh%Mm%Ss")
+
+        readable_dates.append(readable_date)
+        readable_times.append(readable_time)
+
+    df['readable_date'] = readable_dates
+    df['readable_time'] = readable_times
+
+    df['status_website'] = None
+    df['login']	= None
+    df['submitted'] = None
+    df['tentativas'] =None
+    df['qualtrics'] = None
+
+    df.to_csv(output_csv, sep='\t', index=False)
+
+if __name__ == "__main__":
+    input_folder = '/media/labadmin/Windows/Users/diogo/aixlab/data/raw/'
+    output_csv = 'cyclesix_empty.csv'
+    match_string = 'cyclesix_screen'  # Change this to None if you don't want to filter by string
+
+    main(input_folder, output_csv, match_string)
